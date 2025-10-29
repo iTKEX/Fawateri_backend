486c96d625e171dc6e795be68246fbe2b37551a1
turki <tkex_@outlook.com>
2025-10-29 11:29:37 +0300

    Register Category, Reminder and Image models to admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Bill
+from .models import Bill, Category, Reminder, Image
 
 # Register your models here.
 admin.site.register(Bill)
+admin.site.register(Category)
+admin.site.register(Reminder)
+admin.site.register(Image)
