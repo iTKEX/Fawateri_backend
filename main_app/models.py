1ca12b4a064393b5ec8e7697c789b023e3b9327e
turki <tkex_@outlook.com>
2025-10-29 09:55:13 +0300

    Create Bill model, Bill Serializer, Testing Create Bill from Admin dashboard

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Bill(models.Model):
+    company = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=250)
+    created_at = models.DateField(auto_now_add=True)
+    warranty = models.CharField(max_length=50, blank=True)
+    cost = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return f'{self.company}, {self.title}'
