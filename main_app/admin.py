10f6256529cc3c42330cbbf8a2962888220047a4
turki <turki@Hisham-laptop.localdomain>
2025-11-03 09:36:33 +0300

    clean up admin.py structure and comments

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,20 @@
+"""
+Table of Contents
+- Imports
+- Model Registration
+"""
+
+# ========================
+# - Imports
+# ========================
 from django.contrib import admin
 from .models import Bill, Category, Reminder, Image
 
-# Register your models here.
+# ========================
+# - Model Registration
+# ========================
+
+# Register models to appear in Django Admin site.
 admin.site.register(Bill)
 admin.site.register(Category)
 admin.site.register(Reminder)
